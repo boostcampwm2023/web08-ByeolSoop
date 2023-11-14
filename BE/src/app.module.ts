a4c380c4f10716754af2e964556b93ccb9348284
jeongmin <yesuel1111@naver.com>
2023-11-14 13:34:54 +0900

    Feat: TypeORM 설치 및 config 파일 추가
    Co-authored-by: Joonsoo Kim <kjsoo1901@gmail.com>

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeORMConfig } from "./configs/typeorm.config";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeORMConfig)],
 })
 export class AppModule {}
