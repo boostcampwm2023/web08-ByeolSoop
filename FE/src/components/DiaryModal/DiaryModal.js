c459cc9851029e5935e66bd34f2f87dc4fce9e83
dmson1218 <dmson1218@naver.com>
2023-11-14 19:38:23 +0900

    feat: 태그 추가 박스 추가
    - 제목 입력 박스 컴포넌트 재사용

--- FE/src/components/DiaryModal/DiaryModal.js
+++ FE/src/components/DiaryModal/DiaryModal.js
@@ -4,9 +4,25 @@ import styled from "styled-components";
 function DiaryModal() {
   return (
     <DiaryModalWrapper>
-      <DiaryModalTitle>새로운 별의 이야기를 적어주세요.</DiaryModalTitle>
-      <DiaryModalInputBox height='3rem' placeholder='제목을 입력해주세요.' />
-      <DiaryModalInputBox height='33rem' placeholder='내용을 입력해주세요.' />
+      <DiaryModalHeader>
+        <DiaryModalTitle>새로운 별의 이야기를 적어주세요.</DiaryModalTitle>
+        <DiaryModalDate>
+          {new Date().toLocaleDateString("ko-KR", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </DiaryModalDate>
+      </DiaryModalHeader>
+      <DiaryModalInputBox
+        fontSize='1.1rem'
+        placeholder='제목을 입력해주세요.'
+      />
+      <DiaryModalContentInputBox
+        height='30rem'
+        placeholder='내용을 입력해주세요.'
+      />
+      <DiaryModalInputBox fontSize='1rem' placeholder='태그를 입력해주세요.' />
       <DiaryModalButton>저장하기</DiaryModalButton>
     </DiaryModalWrapper>
   );
@@ -31,23 +47,59 @@ const DiaryModalWrapper = styled.div`
   color: #ffffff;
 `;
 
-const DiaryModalTitle = styled.h1`
+const DiaryModalHeader = styled.div`
   width: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
 
+const DiaryModalTitle = styled.h1`
   font-size: 1.5rem;
 `;
 
+const DiaryModalDate = styled.div`
+  color: rgba(0, 0, 0, 0.55);
+`;
+
 const DiaryModalInputBox = styled.input`
+  width: 100%;
+  height: 3rem;
+  border-radius: 0.2rem;
+  border: 1px solid #ffffff;
+  background-color: transparent;
+  padding: 1rem;
+  box-sizing: border-box;
+  color: #ffffff;
+  outline: none;
+
+  font-family: "Pretendard-Medium";
+  font-size: ${(props) => props.fontSize}};
+
+  &::placeholder {
+    color: #ffffff;
+  }
+`;
+
+const DiaryModalContentInputBox = styled.textarea`
   width: 100%;
   height: ${(props) => props.height};
   border-radius: 0.2rem;
   border: 1px solid #ffffff;
   background-color: transparent;
-  padding: 0 1rem;
+  padding: 1rem;
   box-sizing: border-box;
   color: #ffffff;
   outline: none;
+
+  font-family: "Pretendard-Medium";
   font-size: 1rem;
+  line-height: 1.5rem;
+
+  resize: none;
+
+  word_wrap: break-word;
+  word-break: break-all;
 
   &::placeholder {
     color: #ffffff;
