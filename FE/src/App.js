20fbf556ea7ff68de26e19a473d0381ae164e8af
dmson1218 <dmson1218@naver.com>
2023-11-14 13:54:09 +0900

    feat: 전역 CSS 설정
    - styled-reset 설치
    - Pretendard 폰트 설치
    - Pretendard 폰트 전역 스타일 적용

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
 import MainPage from "./pages/MainPage";
+import "./assets/fonts/fonts.css";
+
+const GlobalStyle = createGlobalStyle`
+  ${Reset}
+  body {
+    font-family: "Pretendard-Medium";
+  }
+`;
 
 function App() {
   return (
     <div className='App'>
+      <GlobalStyle />
       <MainPage />
     </div>
   );
