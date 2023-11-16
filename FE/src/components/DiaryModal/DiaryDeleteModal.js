c743c479d9b0ff97d307277361ec4f8b97593162
dmson1218 <dmson1218@naver.com>
2023-11-16 17:39:48 +0900

    feat: 일기 삭제 모달 UI
    - DiaryDeleteModal 컴포넌트 구현

---
+++ FE/src/components/DiaryModal/DiaryDeleteModal.js
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import { useSetRecoilState } from "recoil";
+import diaryAtom from "../../atoms/diaryAtom";
+import ModalWrapper from "../../styles/Modal/ModalWrapper";
+
+function DiaryDeleteModal() {
+  const setDiaryState = useSetRecoilState(diaryAtom);
+
+  return (
+    <DeleteModalWrapper left='50%' width='15vw' height='10vh' opacity='0'>
+      <DeleteModalTitle>삭제하시겠습니까?</DeleteModalTitle>
+      <DeleteModalButtonWrapper>
+        <DeleteModalButton
+          onClick={() => {
+            setDiaryState({
+              isCreate: false,
+              isRead: true,
+              isDelete: false,
+            });
+          }}
+        >
+          취소
+        </DeleteModalButton>
+        <DeleteModalButton>확인</DeleteModalButton>
+      </DeleteModalButtonWrapper>
+    </DeleteModalWrapper>
+  );
+}
+
+const DeleteModalWrapper = styled(ModalWrapper)`
+  background-color: #bbc2d4;
+  color: #000000;
+`;
+
+const DeleteModalTitle = styled.div`
+  font-size: 1.5rem;
+  margin-bottom: 1.5rem;
+  text-align: center;
+  align-self: center;
+`;
+
+const DeleteModalButtonWrapper = styled.div`
+  width: 200%;
+  position: relative;
+  top: 10%;
+  left: -50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const DeleteModalButton = styled.button`
+  width: 30%;
+  height: 3rem;
+  border: none;
+  border-radius: 0.5rem;
+  font-family: "Pretendard-Medium";
+  font-size: 1.2rem;
+  cursor: pointer;
+`;
+
+export default DiaryDeleteModal;
