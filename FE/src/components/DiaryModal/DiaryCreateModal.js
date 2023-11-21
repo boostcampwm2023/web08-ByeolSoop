14d1bb01f281588d9ba3dba9513bce8ab4c31576
dmson1218 <dmson1218@naver.com>
2023-11-21 16:12:21 +0900

    feat: 태그 박스 UI 수정
    - TagList 배열의 값을 불러와 DiaryModalTagBox 컴포넌트 추가
    - 사용자가 입력 후 엔터, 혹은 포커스 해제 시 TagList에 해당 Tag 추가
    - 사용자가 컴포넌트 클릭, 혹은 백스페이스 입력 시 TagList에서 해당 Tag 삭제

--- FE/src/components/DiaryModal/DiaryCreateModal.js
+++ FE/src/components/DiaryModal/DiaryCreateModal.js
@@ -6,9 +6,28 @@ import ModalWrapper from "../../styles/Modal/ModalWrapper";
 import DiaryModalHeader from "../../styles/Modal/DiaryModalHeader";
 import stars from "../../assets/stars";
 
+function pushTag(tagList, tag) {
+  if (tagList.includes(tag)) {
+    return tagList;
+  }
+  return [...tagList, tag];
+}
+
 function DiaryCreateModal() {
   const [isInput, setIsInput] = React.useState(false);
+  const [tagList, setTagList] = React.useState([]);
   const setDiaryState = useSetRecoilState(diaryAtom);
+
+  const addTag = (e) => {
+    if (e.target.value.length > 0) {
+      setTagList(pushTag(tagList, e.target.value));
+      e.target.value = "";
+    }
+  };
+
+  const deleteTag = () => {
+    setTagList(tagList.slice(0, tagList.length - 1));
+  };
 
   return (
     <ModalWrapper left='60%' width='40vw' height='65vh' opacity='0.3'>
@@ -34,7 +53,33 @@ function DiaryCreateModal() {
         }}
       />
       <DiaryModalContentInputBox placeholder='내용을 입력해주세요.' />
-      <DiaryModalInputBox fontSize='1rem' placeholder='태그를 입력해주세요.' />
+      <DiaryModalTagWrapper>
+        {tagList.map((tag) => (
+          <DiaryModalTagBox
+            onClick={() => {
+              setTagList(tagList.filter((item) => item !== tag));
+            }}
+          >
+            {tag}
+          </DiaryModalTagBox>
+        ))}
+        <DiaryModalTagInputBox
+          fontSize='1rem'
+          placeholder='태그를 입력해주세요.'
+          onBlur={(e) => addTag(e)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              addTag(e);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Backspace" && e.target.value.length === 0) {
+              deleteTag();
+            }
+          }}
+        />
+      </DiaryModalTagWrapper>
+
       <DiaryModalShapeSelectBox />
       <ModalSideButtonWrapper>
         <ModalSideButton
@@ -49,9 +94,7 @@ function DiaryCreateModal() {
           X
         </ModalSideButton>
         {isInput ? (
-          <ModalSideButton width='5rem' borderRadius='2rem'>
-            생성
-          </ModalSideButton>
+          <DiaryModalTagBox borderRadius='2rem'>생성</DiaryModalTagBox>
         ) : null}
       </ModalSideButtonWrapper>
     </ModalWrapper>
@@ -205,6 +248,59 @@ const DiaryModalContentInputBox = styled.textarea`
   &::placeholder {
     color: #ffffff;
   }
+`;
+
+const DiaryModalTagWrapper = styled.div`
+  width: 100%;
+
+  border-radius: 0.2rem;
+  border: 1px solid #ffffff;
+  background-color: transparent;
+  padding: 0.5rem 1rem;
+  box-sizing: border-box;
+  color: #ffffff;
+  outline: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const DiaryModalTagInputBox = styled.input`
+  width: 8.5rem;
+  padding: 0.5rem 0;
+  border: none;
+  background-color: transparent;
+  color: #ffffff;
+  outline: none;
+
+  flex-grow: 1;
+
+  font-family: "Pretendard-Medium";
+  font-size: 1rem;
+
+  &::placeholder {
+    color: #ffffff;
+  }
+`;
+
+const DiaryModalTagBox = styled.div`
+  padding: 0.5rem 1rem;
+  border-radius: 1.5rem;
+  border: 1px solid #ffffff;
+  background-color: rgba(255, 255, 255, 0.3);
+
+  flex-shrink: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
 `;
 
 export default DiaryCreateModal;
