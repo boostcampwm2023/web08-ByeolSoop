696fbf16c12fbd0ec91545f45afadeb120fd27aa
jeongmin <yesuel1111@naver.com>
2023-11-21 23:27:57 +0900

    Feat: NoDuplicateLoginGuard 구현
    - 로그인 상태(만료되지 않은 Access Token을 가지고 오는 경우)에 로그인 API 요청 시 409 Conflict 반환

--- BE/src/auth/auth.controller.ts
+++ BE/src/auth/auth.controller.ts
@@ -1,13 +1,21 @@
-import { Body, Controller, Post, ValidationPipe } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { AccessTokenDto } from "./dto/auth-access-token.dto";
+import { NoDuplicateLoginGuard } from "./guard/auth.user-guard";
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post("/signin")
+  @UseGuards(NoDuplicateLoginGuard)
   signIn(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<AccessTokenDto> {
