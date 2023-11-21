b76fec32efe91855458bfec5d5f12b11d6d50127
JoonSoo-Kim <kjsoo1901@gmail.com>
2023-11-21 18:34:41 +0900

    style: typeorm.config.ts 파일 분리
    - typeorm.config.ts파일에 있던 typeORMTestConfig를 typeorm.test.config.ts 파일을 생성하여 이동

--- BE/src/configs/typeorm.test.config.ts
+++ BE/src/configs/typeorm.test.config.ts
@@ -1,14 +1,14 @@
 import "dotenv/config";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-export const typeORMConfig: TypeOrmModuleOptions = {
+export const typeORMTestConfig: TypeOrmModuleOptions = {
   type: "mysql",
   host: process.env.DB_HOST,
   port: 3306,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  entities: ["dist/**/*.entity{.ts,.js}"],
-  synchronize: false,
+  database: process.env.TEST_DB_NAME,
+  entities: ["src/**/*.entity{.ts,.js}"],
+  synchronize: true,
   timezone: "+09:00",
 };
