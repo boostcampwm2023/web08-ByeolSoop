cbd82ae089ad584d36abe5d9c199aa4e914634c1
JoonSoo-Kim <kjsoo1901@gmail.com>
2023-11-21 23:15:23 +0900

    test: 일기 생성 시 빈 값을 포함한 요청 e2e 테스트 작성
    - 일기 생성에 대해 빈 값을 포함하여 요청 시 400 Bad Request와 적절한 오류 메세지를 응답하는 e2e 테스트 작성

--- BE/test/diaries.create.e2e-spec.ts
+++ BE/test/diaries.create.e2e-spec.ts
@@ -69,4 +69,93 @@ describe("AppController (e2e)", () => {
 
     expect(body.message).toBe("Unauthorized");
   });
+
+  it("빈 값을 포함한 요청 시 400 Bad Request 응답", async () => {
+    let postResponse;
+    let body;
+    // 제목이 없는 경우
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        content: "this is content.",
+        point: "1.5,5.5,10.55",
+        date: "2023-11-14",
+        tags: ["tagTest", "tagTest2"],
+        shapeUuid: "0c99bbc6-e404-464b-a310-5bf0fa0f0fa7",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("제목은 비어있지 않아야 합니다.");
+
+    // 좌표가 없는 경우
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: "this is content.",
+        date: "2023-11-14",
+        tags: ["tagTest", "tagTest2"],
+        shapeUuid: "0c99bbc6-e404-464b-a310-5bf0fa0f0fa7",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("좌표는 비어있지 않아야 합니다.");
+
+    // 날짜가 없는 경우
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: "this is content.",
+        point: "1.5,5.5,10.55",
+        tags: ["tagTest", "tagTest2"],
+        shapeUuid: "0c99bbc6-e404-464b-a310-5bf0fa0f0fa7",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("날짜는 비어있지 않아야 합니다.");
+
+    // 모양 uuid가 없는 경우
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: "this is content.",
+        point: "1.5,5.5,10.55",
+        date: "2023-11-14",
+        tags: ["tagTest", "tagTest2"],
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("모양 uuid는 비어있지 않아야 합니다.");
+
+    // 복수의 데이터가 없는 경우
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: "this is content.",
+        tags: ["tagTest", "tagTest2"],
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("날짜는 비어있지 않아야 합니다.");
+    expect(body.message).toContain("좌표는 비어있지 않아야 합니다.");
+    expect(body.message).toContain("모양 uuid는 비어있지 않아야 합니다.");
+  });
 });
