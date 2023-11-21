9003f17584fb835a57693a94781f834bb631b942
JoonSoo-Kim <kjsoo1901@gmail.com>
2023-11-21 23:16:19 +0900

    test: 일기 생성 시 부적절한 값으로 요청 e2e 테스트 작성
    - 일기 생성에 대해 부적절한 값을 포함하여 요청 시 400 Bad Request와 적절한 오류 메세지를 응답하는 e2e 테스트 작성

--- BE/test/diaries.create.e2e-spec.ts
+++ BE/test/diaries.create.e2e-spec.ts
@@ -158,4 +158,134 @@ describe("AppController (e2e)", () => {
     expect(body.message).toContain("좌표는 비어있지 않아야 합니다.");
     expect(body.message).toContain("모양 uuid는 비어있지 않아야 합니다.");
   });
+
+  it("부적절한 값으로 요청 시 400 Bad Request 응답", async () => {
+    let postResponse;
+    let body;
+    // 문자열이 아닌 제목으로 요청
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: 35,
+        content: "this is content.",
+        point: "1.5,5.5,10.55",
+        date: "2023-11-14",
+        tags: ["tagTest", "tagTest2"],
+        shapeUuid: "0c99bbc6-e404-464b-a310-5bf0fa0f0fa7",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("제목은 문자열이어야 합니다.");
+
+    // 문자열이 아닌 내용으로 요청
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: 35,
+        point: "1.5,5.5,10.55",
+        date: "2023-11-14",
+        tags: ["tagTest", "tagTest2"],
+        shapeUuid: "0c99bbc6-e404-464b-a310-5bf0fa0f0fa7",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("내용은 문자열이어야 합니다.");
+
+    // 문자열이 아닌 좌표로 요청
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: "this is content.",
+        point: [],
+        date: "2023-11-14",
+        tags: ["tagTest", "tagTest2"],
+        shapeUuid: "0c99bbc6-e404-464b-a310-5bf0fa0f0fa7",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("좌표는 문자열이어야 합니다.");
+
+    // 적절하지 않은 좌표 양식의 문자열로 요청
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: "this is content.",
+        point: "-1.5.5.5.10.55",
+        date: "2023-11-14",
+        tags: ["tagTest", "tagTest2"],
+        shapeUuid: "0c99bbc6-e404-464b-a310-5bf0fa0f0fa7",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("적절하지 않은 좌표 양식입니다.");
+
+    // 적절하지 않은 날짜 양식의 문자열로 요청
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: "this is content.",
+        point: "1.5,5.5,10.55",
+        date: "20231114019",
+        tags: ["tagTest", "tagTest2"],
+        shapeUuid: "0c99bbc6-e404-464b-a310-5bf0fa0f0fa7",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("date must be a valid ISO 8601 date string");
+
+    // 배열 형태가 아닌 태그로 요청
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: "this is content.",
+        point: "1.5,5.5,10.55",
+        date: "2023-11-14",
+        tags: "tagTest2",
+        shapeUuid: "0c99bbc6-e404-464b-a310-5bf0fa0f0fa7",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("태그는 배열의 형태여야 합니다.");
+
+    // uuid 양식이 아닌 모양 uuid로 요청
+    postResponse = await request(app.getHttpServer())
+      .post("/diaries")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        title: "title",
+        content: "this is content.",
+        point: "1.5,5.5,10.55",
+        date: "2023-11-14",
+        tags: ["tagTest", "tagTest2"],
+        shapeUuid: "uuidTest",
+      })
+      .expect(400);
+
+    body = JSON.parse(postResponse.text);
+
+    expect(body.message).toContain("모양 uuid 값이 uuid 양식이어야 합니다.");
+  });
 });
