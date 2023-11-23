4dd30398120a12ac7906314e02d1fef0f0afb3ac
Joonsoo Kim <kjsoo1901@gmail.com>
2023-11-23 17:40:00 +0900

    refactor: 사용자 DTO 수정
    - 현재 사용하지 않는 LoginUserDto 삭제
    - CreateUserDto의 class validator에 적절한 오류 메세지 추가

--- BE/src/auth/dto/users.dto.ts
+++ BE/src/auth/dto/users.dto.ts
@@ -1,31 +1,35 @@
-import { IsString, Length, MaxLength, Matches } from "class-validator";
+import {
+  IsString,
+  Length,
+  MaxLength,
+  Matches,
+  IsNotEmpty,
+} from "class-validator";
 
 export class CreateUserDto {
-  @IsString()
-  @Matches(/^[A-Za-z0-9-]{5,20}$/)
+  @IsNotEmpty({ message: "아이디는 비어있지 않아야 합니다." })
+  @IsString({ message: "아이디는 문자열이어야 합니다." })
+  @Matches(/^[A-Za-z0-9-]{5,20}$/, {
+    message: "생성 규칙에 맞지 않는 아이디입니다.",
+  })
   userId: string;
 
-  @IsString()
+  @IsNotEmpty({ message: "이메일은 비어있지 않아야 합니다." })
+  @IsString({ message: "이메일은 문자열이어야 합니다." })
   @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
     message: "적절하지 않은 이메일 양식입니다.",
   })
   email: string;
 
-  @IsString()
-  @Matches(/^[A-Za-z0-9!@#$%^&*()+=-~]{5,20}$/)
+  @IsNotEmpty({ message: "비밀번호는 비어있지 않아야 합니다." })
+  @IsString({ message: "비밀번호는 문자열이어야 합니다." })
+  @Matches(/^[A-Za-z0-9!@#$%^&*()+=-~]{5,20}$/, {
+    message: "생성 규칙에 맞지 않는 비밀번호 입니다.",
+  })
   password: string;
 
-  @IsString()
-  @MaxLength(21)
+  @IsNotEmpty({ message: "닉네임은 비어있지 않아야 합니다." })
+  @IsString({ message: "닉네임은 문자열이어야 합니다." })
+  @MaxLength(21, { message: "닉네임은 20자 이하여야 합니다." })
   nickname: string;
-}
-
-export class LoginUserDto {
-  @IsString()
-  @Length(5, 20)
-  userId: string;
-
-  @IsString()
-  @Length(5, 20)
-  password: string;
 }
