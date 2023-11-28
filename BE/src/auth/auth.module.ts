46a8c574dbc18a9d74416df289b60097208b280a
JoonSoo-Kim <kjsoo1901@gmail.com>
2023-11-28 11:11:55 +0900

    style: 사용하지 않는 함수 제거
    - 현재 사용하지 않는 forwardRef 제거

--- BE/src/auth/auth.module.ts
+++ BE/src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { Module, forwardRef } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtModule } from "@nestjs/jwt";
