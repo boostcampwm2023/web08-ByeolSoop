265227e28c029325b739fc4209ad6a859eaef90b
dbwhdtjr0457 <dbwhdtjr0457@khu.ac.kr>
2023-11-29 16:03:17 +0900

    feat: svg 형태로 데이터 불러오고 출력하기 구현

--- FE/src/pages/MainPage.js
+++ FE/src/pages/MainPage.js
@@ -1,7 +1,11 @@
+/* eslint-disable */
+
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState, useRecoilValue } from "recoil";
 import diaryAtom from "../atoms/diaryAtom";
+import shapeAtom from "../atoms/shapeAtom";
+import userAtom from "../atoms/userAtom";
 import DiaryCreateModal from "../components/DiaryModal/DiaryCreateModal";
 import DiaryReadModal from "../components/DiaryModal/DiaryReadModal";
 import background from "../assets/background.png";
@@ -11,6 +15,8 @@ import DiaryLoadingModal from "../components/DiaryModal/DiaryLoadingModal";
 
 function MainPage() {
   const [diaryState, setDiaryState] = useRecoilState(diaryAtom);
+  const userState = useRecoilValue(userAtom);
+  const setShapeState = useSetRecoilState(shapeAtom);
 
   useEffect(() => {
     setDiaryState((prev) => {
@@ -24,6 +30,34 @@ function MainPage() {
       window.history.pushState(newState, "", "");
       return newState;
     });
+
+    async function getShapeFn() {
+      return fetch("http://223.130.129.145:3005/shapes/default", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then(async (data) => {
+          const shapeDataList = data.map((shape) =>
+            fetch(`http://223.130.129.145:3005/shapes/${shape.uuid}`, {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userState.accessToken}`,
+              },
+            }).then(async (res) => ({
+              data: await res.text(),
+            })),
+          );
+
+          console.log(await Promise.all(shapeDataList));
+          setShapeState(await Promise.all(shapeDataList));
+        });
+    }
+
+    getShapeFn();
   }, []);
 
   return (
@@ -40,6 +74,20 @@ function MainPage() {
           }));
         }}
       />
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 100 100'
+        width='4rem'
+        height='4rem'
+      >
+        <defs>
+          <style>{`.cls-1{fill:none;stroke:gray;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.06px;}`}</style>
+        </defs>
+        <polygon
+          className='cls-1'
+          points='50 23.94 58.31 41.69 76.06 50 58.31 58.31 50 76.06 41.69 58.31 23.94 50 41.69 41.69 50 23.94'
+        />
+      </svg>
       {diaryState.isCreate ? <DiaryCreateModal /> : null}
       {diaryState.isRead ? <DiaryReadModal /> : null}
       {diaryState.isUpdate ? <DiaryUpdateModal /> : null}
