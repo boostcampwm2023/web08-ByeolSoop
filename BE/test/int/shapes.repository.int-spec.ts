17fae799f5c69804b18a90dca72abd5df212f7e8
JoonSoo-Kim <kjsoo1901@gmail.com>
2023-11-29 20:52:45 +0900

    test: ShapesService, ShapesRepository 통합 테스트 작성
    - ShapesService 통합 테스트 작성
    - ShapesRepository 통합 테스트 작성

---
+++ BE/test/int/shapes.repository.int-spec.ts
@@ -0,0 +1,98 @@
+import { RedisModule } from "@liaoliaots/nestjs-redis";
+import { NotFoundException } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "src/auth/auth.module";
+import { User } from "src/auth/users.entity";
+import { typeORMTestConfig } from "src/configs/typeorm.test.config";
+import { Shape } from "src/shapes/shapes.entity";
+import { ShapesModule } from "src/shapes/shapes.module";
+import { ShapesRepository } from "src/shapes/shapes.repository";
+import { DataSource } from "typeorm";
+
+describe("ShapesRepository", () => {
+  let shapesRepository: ShapesRepository;
+  let dataSource: DataSource;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(typeORMTestConfig),
+        ShapesModule,
+        RedisModule.forRoot({
+          readyLog: true,
+          config: {
+            host: "223.130.129.145",
+            port: 6379,
+          },
+        }),
+      ],
+      providers: [ShapesRepository],
+    }).compile();
+
+    shapesRepository =
+      await moduleFixture.get<ShapesRepository>(ShapesRepository);
+  });
+
+  afterAll(async () => {});
+
+  describe("createDefaultShapes 통합 테스트", () => {
+    it("메서드 정상 요청", async () => {
+      const commonUser = await User.findOne({
+        where: { userId: "commonUser" },
+      });
+
+      await shapesRepository.createDefaultShapes(commonUser);
+
+      // 응답이 없는 메서드는 어떻게 테스트해야 할까?
+    });
+  });
+
+  describe("getShapesByUser 통합 테스트", () => {
+    it("메서드 정상 요청", async () => {
+      const commonUser = await User.findOne({
+        where: { userId: "commonUser" },
+      });
+
+      const result: Shape[] =
+        await shapesRepository.getShapesByUser(commonUser);
+
+      // 현재는 기본 모양 15개와 기존에 임시로 사용하던 기본 모양 3개가 모두 있음
+      expect(result.length).toBe(18);
+    });
+  });
+
+  describe("getShapeByShapePath 통합 테스트", () => {
+    it("메서드 정상 요청", async () => {
+      const shape = await Shape.findOne({
+        where: {
+          user: {
+            userId: "commonUser",
+          },
+        },
+      });
+
+      const result = await shapesRepository.getShapeByShapePath(
+        shape.shapePath,
+      );
+
+      expect(result).toStrictEqual(shape);
+    });
+  });
+
+  describe("getShapeByUuid 통합 테스트", () => {
+    it("메서드 정상 요청", async () => {
+      const shape = await Shape.findOne({
+        where: {
+          user: {
+            userId: "commonUser",
+          },
+        },
+      });
+
+      const result = await shapesRepository.getShapeByUuid(shape.uuid);
+
+      expect(result).toStrictEqual(shape);
+    });
+  });
+});
