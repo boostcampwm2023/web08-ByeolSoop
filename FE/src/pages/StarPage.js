27696e2fdc3fa56fd512073d0f49c0ad8bc65cfc
dmson1218 <dmson1218@naver.com>
2023-11-30 17:31:42 +0900

    feat: 별 형태 수정
    - 구에서 사용자가 선택한 2차원 이미지 평면으로 수정
    - 일기 내용 감정 상태에 따른 후광 그라디언트 설정

--- FE/src/pages/StarPage.js
+++ FE/src/pages/StarPage.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/no-unknown-property */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { Canvas, useThree } from "@react-three/fiber";
-import { Sphere, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 import diaryAtom from "../atoms/diaryAtom";
+import shapeAtom from "../atoms/shapeAtom";
 import starAtom from "../atoms/starAtom";
 import SwitchButton from "../components/Button/SwitchButton";
 import ModalWrapper from "../styles/Modal/ModalWrapper";
@@ -17,6 +18,7 @@ import paint from "../assets/paint.svg";
 
 function StarPage() {
   const [starState, setStarState] = useRecoilState(starAtom);
+
   return (
     <>
       <CanvasContainer>
@@ -30,7 +32,6 @@ function StarPage() {
             enabled={starState.drag}
             enablePan={false}
             enableDamping={false}
-            enableZoom={false}
             target={[0, 0, 0]}
           />
           <StarView />
@@ -92,6 +93,28 @@ function StarView() {
   const { scene, raycaster, camera } = useThree();
   const [diaryState, setDiaryState] = useRecoilState(diaryAtom);
   const { mode, points } = useRecoilValue(starAtom);
+  const shapeState = useRecoilValue(shapeAtom);
+  const [texture, setTexture] = useState({});
+
+  useEffect(() => {
+    const newTexture = {};
+    shapeState.forEach((shape) => {
+      const blob = new Blob([shape.data], { type: "image/svg+xml" });
+      const urlObject = URL.createObjectURL(blob);
+      const loader = new THREE.TextureLoader();
+      const svgTexture = loader.load(urlObject);
+      newTexture[shape.uuid] = svgTexture;
+    });
+    setTexture(newTexture);
+  }, [shapeState]);
+
+  // const shapeData = shapeState.find((shape)
+  // => shape.uuid === shapeUuid)?.data;
+  // const blob = new Blob([shapeData], { type: "image/svg+xml" });
+  // const urlObject = URL.createObjectURL(blob);
+
+  // const loader = new THREE.TextureLoader();
+  // const svgTexture = loader.load(urlObject);
 
   const material = new THREE.ShaderMaterial({
     uniforms: {
@@ -175,31 +198,39 @@ function StarView() {
 
   return (
     <>
+      <mesh>
+        <sphereGeometry
+          args={[30, 32, 16, 0, Math.PI * 2, 0, Math.PI / 1.98]}
+        />
+        <primitive object={material} attach='material' side={THREE.BackSide} />
+      </mesh>
       <mesh
         onClick={mode === "create" ? clickOnCreate : null}
         onDoubleClick={mode === "create" ? doubleClickOnCreate : null}
       >
-        <sphereGeometry
-          args={[30, 64, 32, 0, Math.PI * 2, 0, Math.PI / 1.98]}
-        />
-        <primitive object={material} attach='material' side={THREE.BackSide} />
+        <sphereGeometry args={[29]} />
+        <meshStandardMaterial transparent opacity={0} side={THREE.BackSide} />
       </mesh>
       <mesh>
         <sphereGeometry args={[3]} />
         <meshStandardMaterial transparent opacity={0} side={THREE.BackSide} />
       </mesh>
-      {diaryState.diaryList?.map((diary) => (
-        <Star
-          key={[diary.coordinate.x, diary.coordinate.y, diary.coordinate.z]}
-          uuid={diary.uuid}
-          position={[
-            diary.coordinate.x,
-            diary.coordinate.y,
-            diary.coordinate.z,
-          ]}
-          moveToStar={moveToStar}
-        />
-      ))}
+      {Object.keys(texture).length > 0
+        ? diaryState.diaryList?.map((diary) => (
+            <Star
+              key={[diary.coordinate.x, diary.coordinate.y, diary.coordinate.z]}
+              uuid={diary.uuid}
+              position={[
+                diary.coordinate.x,
+                diary.coordinate.y,
+                diary.coordinate.z,
+              ]}
+              sentiment={diary.emotion.sentiment}
+              texture={texture[diary.shapeUuid]}
+              moveToStar={moveToStar}
+            />
+          ))
+        : null}
       {points.map((point) => (
         <Line
           key={[...point[0].position, ...point[1].position]}
@@ -211,7 +242,7 @@ function StarView() {
 }
 
 function Star(props) {
-  const { uuid, position, moveToStar } = props;
+  const { uuid, position, sentiment, texture, moveToStar } = props;
   const setDiaryState = useSetRecoilState(diaryAtom);
   const [starState, setStarState] = useRecoilState(starAtom);
   const { mode, selected } = starState;
@@ -271,25 +302,89 @@ function Star(props) {
     }
   };
 
+  // 긍정 - 00ccff, 부정 - d1180b, 중립 - ba55d3
+  const sentimentColor = {
+    positive: "#00ccff",
+    negative: "#d1180b",
+    neutral: "#ba55d3",
+  }[sentiment];
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      color1: { value: new THREE.Color(sentimentColor) },
+      color2: { value: new THREE.Color("#182683") },
+      gradientStart: { value: 0.001 },
+    },
+    vertexShader: `
+      varying vec2 vUv;
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      uniform vec3 color1;
+      uniform vec3 color2;
+      uniform float gradientStart;
+      varying vec2 vUv;
+
+
+      void main() {
+        float distance = length(vUv - vec2(0.5));
+        vec3 finalColor = mix(
+          color1,
+          color2,
+          smoothstep(gradientStart, gradientStart + 0.57, distance)
+        );
+
+        if (distance < gradientStart) {
+            finalColor = vec3(0.0, 0.0, 0.0);
+        }
+
+        gl_FragColor = vec4(finalColor, 1.0);
+      }
+    `,
+  });
+
   return (
-    <mesh
-      position={position}
-      onPointerEnter={(e) => {
-        e.stopPropagation();
-        e.object.scale.set(1.5, 1.5, 1.5);
-        e.object.material.emissiveIntensity = 1;
-      }}
-      onPointerLeave={(e) => {
-        e.stopPropagation();
-        e.object.scale.set(1, 1, 1);
-        e.object.material.emissiveIntensity = 0.7;
-      }}
-      onClick={mode === "create" ? clickOnCreate : clickOnEdit}
-    >
-      <Sphere args={[0.2]}>
-        <meshStandardMaterial emissive={0xffffff} emissiveIntensity={0.7} />
-      </Sphere>
-    </mesh>
+    <>
+      <mesh
+        rotation={new THREE.Euler().setFromRotationMatrix(
+          new THREE.Matrix4().lookAt(
+            new THREE.Vector3(),
+            new THREE.Vector3(...position),
+            new THREE.Vector3(0, 1, 0), // Up vector
+          ),
+        )}
+        position={position}
+        onPointerEnter={(e) => {
+          e.stopPropagation();
+          e.object.scale.set(1.5, 1.5, 1.5);
+          e.object.material.emissiveIntensity = 1;
+        }}
+        onPointerLeave={(e) => {
+          e.stopPropagation();
+          e.object.scale.set(1, 1, 1);
+          e.object.material.emissiveIntensity = 0.7;
+        }}
+        onClick={mode === "create" ? clickOnCreate : clickOnEdit}
+      >
+        <planeGeometry args={[1.5, 1.5]} />
+        <meshBasicMaterial map={texture} transparent />
+      </mesh>
+      <mesh
+        rotation={new THREE.Euler().setFromRotationMatrix(
+          new THREE.Matrix4().lookAt(
+            new THREE.Vector3(),
+            new THREE.Vector3(...position),
+            new THREE.Vector3(0, 1, 0), // Up vector
+          ),
+        )}
+        position={position.map((p) => p * 1.01)}
+      >
+        <circleGeometry args={[0.6, 32]} />
+        <primitive object={material} attach='material' />
+      </mesh>
+    </>
   );
 }
 
@@ -297,7 +392,9 @@ function Line(props) {
   const { point } = props;
 
   const lineGeometry = new THREE.BufferGeometry().setFromPoints(
-    point.map((p) => new THREE.Vector3(...p.position)),
+    point.map(
+      (each) => new THREE.Vector3(...each.position.map((p) => p * 1.01)),
+    ),
   );
 
   return (
