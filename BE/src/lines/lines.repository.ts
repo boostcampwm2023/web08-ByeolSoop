3a467edceb8e6205f938cfda05be7e5abea59d37
jeongmin <yesuel1111@naver.com>
2023-11-30 15:31:50 +0900

    feat: 별자리선 생성, 조회, 삭제에 대한 LinesRepository 구현

---
+++ BE/src/lines/lines.repository.ts
@@ -0,0 +1,43 @@
+import { Diary } from "src/diaries/diaries.entity";
+import { Line } from "./lines.entity";
+import { User } from "src/auth/users.entity";
+import { NotFoundException } from "@nestjs/common";
+
+export class LinesRepository {
+  async createLine(
+    firstDiary: Diary,
+    secondDiary: Diary,
+    user: User,
+  ): Promise<Line> {
+    const newLine = Line.create({
+      firstDiary,
+      secondDiary,
+      user,
+    });
+    await newLine.save();
+
+    return newLine;
+  }
+
+  async readLinesByUser(user: User): Promise<Line[]> {
+    const linesList = await Line.find({
+      where: { user: { id: user.id } },
+    });
+    return linesList;
+  }
+
+  async deleteDiary(id: number): Promise<void> {
+    const diary = await this.getLineById(id);
+
+    await Line.softRemove(diary);
+  }
+
+  async getLineById(id: number): Promise<Line> {
+    const found = await Line.findOne({ where: { id } });
+    if (!found) {
+      throw new NotFoundException("존재하지 않는 별자리선입니다.");
+    }
+
+    return found;
+  }
+}
