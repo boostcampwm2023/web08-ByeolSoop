ddf1f0e230faf7db82120c19b682b650107c2022
jeongmin <yesuel1111@naver.com>
2023-12-02 18:19:07 +0900

    feat: 연도별 모양 순위 API 구현
    - stat.shapes.dto 생성
    - stat/shapes-rank/:year GET API 구현
    - queryBuilder에서 take 함수를 limit 함수로 변경
    - getFormatResult에서 count를 number로 형변환
    - statService에서 중복된 부분 모듈화

--- BE/src/stat/stat.controller.ts
+++ BE/src/stat/stat.controller.ts
@@ -10,6 +10,7 @@ import { JwtAuthGuard } from "src/auth/guard/auth.jwt-guard";
 import { User } from "src/auth/users.entity";
 import { StatService } from "./stat.service";
 import { StatTagDto } from "./dto/stat.tags.dto";
+import { StatShapeDto } from "./dto/stat.shapes.dto";
 
 @Controller("stat")
 @UseGuards(JwtAuthGuard)
@@ -22,5 +23,13 @@ export class StatController {
     @GetUser() user: User,
   ): Promise<StatTagDto> {
     return this.statService.getTopThreeTagsByUser(year, user.id);
+  }
+
+  @Get("/shapes-rank/:year")
+  async getShapesRank(
+    @Param("year", ParseIntPipe) year: number,
+    @GetUser() user: User,
+  ): Promise<StatShapeDto> {
+    return this.statService.getTopThreeShapesByUser(year, user.id);
   }
 }
--- BE/src/stat/stat.service.ts
+++ BE/src/stat/stat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { Diary } from "src/diaries/diaries.entity";
 import { StatTagDto, TagInfoDto } from "./dto/stat.tags.dto";
+import { ShapeInfoDto, StatShapeDto } from "./dto/stat.shapes.dto";
 
 @Injectable()
 export class StatService {
@@ -8,36 +9,57 @@ export class StatService {
     year: number,
     userId: number,
   ): Promise<StatTagDto> {
-    const result: TagInfoDto[] = await this.fetchTopThreeTagsByUser(
+    const result: TagInfoDto[] = await this.fetchTopThreeByUser<TagInfoDto>(
       year,
       userId,
+      "tags",
+      ["tags.name", "tags.id"],
+      ["tags.name as tag", "tags.id as id"],
     );
     return this.getFormatResult(result);
   }
 
-  private async fetchTopThreeTagsByUser(
+  async getTopThreeShapesByUser(
     year: number,
     userId: number,
-  ): Promise<TagInfoDto[]> {
-    return await Diary.createQueryBuilder("diary")
-      .select("tags.name", "tag")
-      .addSelect("tags.id", "id")
+  ): Promise<StatShapeDto> {
+    const result: ShapeInfoDto[] = await this.fetchTopThreeByUser<ShapeInfoDto>(
+      year,
+      userId,
+      "shape",
+      ["shape.uuid"],
+      ["shape.uuid as uuid"],
+    );
+    return this.getFormatResult(result);
+  }
+
+  private async fetchTopThreeByUser<T extends TagInfoDto | ShapeInfoDto>(
+    year: number,
+    userId: number,
+    joinTable: string,
+    groupByFields: any,
+    selectFields: any,
+  ): Promise<T[]> {
+    return Diary.createQueryBuilder("diary")
+      .select(selectFields)
       .addSelect("COUNT(*)", "count")
-      .innerJoin("diary.tags", "tags")
+      .innerJoin(`diary.${joinTable}`, joinTable)
       .where("diary.user = :userId", { userId })
       .andWhere("YEAR(diary.date) = :year", { year })
-      .groupBy("tags.name")
-      .addGroupBy("tags.id")
+      .groupBy(groupByFields)
       .orderBy("count", "DESC")
-      .take(3)
+      .limit(3)
       .getRawMany();
   }
 
-  private getFormatResult(result: TagInfoDto[]): StatTagDto {
+  private getFormatResult(
+    result: TagInfoDto[] | ShapeInfoDto[],
+  ): StatTagDto | StatShapeDto {
     const keys = ["first", "second", "third"];
     const formattedResult = {};
-    result.forEach((item, index) => {
+    result.forEach((item: TagInfoDto | ShapeInfoDto, index: number) => {
       const rank = index + 1;
+      item.count = Number(item.count);
       formattedResult[keys[index]] = { rank, ...item };
     });
 
