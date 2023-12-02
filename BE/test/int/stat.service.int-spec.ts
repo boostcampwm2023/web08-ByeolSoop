c632c8995b646a2d6ab636e3cf11d47de14d2b56
jeongmin <yesuel1111@naver.com>
2023-12-02 18:44:00 +0900

    test: getTopThreeShapesByUser 정상 요청 테스트 작성

--- BE/test/int/stat.service.int-spec.ts
+++ BE/test/int/stat.service.int-spec.ts
@@ -27,7 +27,7 @@ describe("StatService 통합 테스트", () => {
         groupBy: jest.fn().mockReturnThis(),
         addGroupBy: jest.fn().mockReturnThis(),
         orderBy: jest.fn().mockReturnThis(),
-        take: jest.fn().mockReturnThis(),
+        limit: jest.fn().mockReturnThis(),
         getRawMany: jest.fn().mockResolvedValue([
           { tag: "태그1", id: 1, count: 10 },
           { tag: "태그2", id: 2, count: 9 },
@@ -45,6 +45,48 @@ describe("StatService 통합 테스트", () => {
         first: { rank: 1, tag: "태그1", id: 1, count: 10 },
         second: { rank: 2, tag: "태그2", id: 2, count: 9 },
         third: { rank: 3, tag: "태그3", id: 3, count: 8 },
+      });
+    });
+  });
+
+  describe("getTopThreeShapesByUser 메서드", () => {
+    it("메서드 정상 요청", async () => {
+      const year = 2023;
+      const userId = 1;
+
+      const mockQueryBuilder = {
+        select: jest.fn().mockReturnThis(),
+        addSelect: jest.fn().mockReturnThis(),
+        innerJoin: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockReturnThis(),
+        groupBy: jest.fn().mockReturnThis(),
+        addGroupBy: jest.fn().mockReturnThis(),
+        orderBy: jest.fn().mockReturnThis(),
+        limit: jest.fn().mockReturnThis(),
+        getRawMany: jest.fn().mockResolvedValue([
+          { uuid: "5d94024c-0f41-4e42-b766-a4f298766f67", count: 6 },
+          { uuid: "5c670cee-f1e1-42ee-9b97-1ff5f561049e", count: 5 },
+        ]),
+      };
+
+      jest
+        .spyOn(Diary, "createQueryBuilder")
+        .mockReturnValue(mockQueryBuilder as any);
+
+      const result = await service.getTopThreeShapesByUser(year, userId);
+
+      expect(result).toEqual({
+        first: {
+          rank: 1,
+          uuid: "5d94024c-0f41-4e42-b766-a4f298766f67",
+          count: 6,
+        },
+        second: {
+          rank: 2,
+          uuid: "5c670cee-f1e1-42ee-9b97-1ff5f561049e",
+          count: 5,
+        },
       });
     });
   });
