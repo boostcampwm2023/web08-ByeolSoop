c5cd36eb0be5a9eb7dc9a428cba00904f5fb2bcf
JoonSoo-Kim <kjsoo1901@gmail.com>
2023-12-03 21:01:55 +0900

    feat: 특정 연도에 대한 날짜별 일기 작성 통계 API 구현
    - 특정 연도에 대한 날짜별 일기 작성 통계 API 구현

--- BE/src/stat/stat.controller.ts
+++ BE/src/stat/stat.controller.ts
@@ -9,7 +9,7 @@ import { GetUser } from "src/auth/get-user.decorator";
 import { JwtAuthGuard } from "src/auth/guard/auth.jwt-guard";
 import { User } from "src/auth/users.entity";
 import { StatService } from "./stat.service";
-import { StatTagDto } from "./dto/stat.tags.dto";
+import { DiariesDateDto, StatTagDto } from "./dto/stat.tags.dto";
 
 @Controller("stat")
 @UseGuards(JwtAuthGuard)
@@ -22,5 +22,13 @@ export class StatController {
     @GetUser() user: User,
   ): Promise<StatTagDto> {
     return this.statService.getTopThreeTagsByUser(year, user.id);
+  }
+
+  @Get("/diaries/:year")
+  async getDiariesDate(
+    @Param("year", ParseIntPipe) year: number,
+    @GetUser() user: User,
+  ): Promise<DiariesDateDto> {
+    return this.statService.getDiariesDateByUser(year, user.id);
   }
 }
--- BE/src/stat/stat.service.ts
+++ BE/src/stat/stat.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { Diary } from "src/diaries/diaries.entity";
-import { StatTagDto, TagInfoDto } from "./dto/stat.tags.dto";
+import {
+  DiariesDateDto,
+  DiariesInfoDto,
+  StatTagDto,
+  TagInfoDto,
+} from "./dto/stat.tags.dto";
 
 @Injectable()
 export class StatService {
@@ -13,6 +18,29 @@ export class StatService {
       userId,
     );
     return this.getFormatResult(result);
+  }
+
+  async getDiariesDateByUser(
+    year: number,
+    userId: number,
+  ): Promise<DiariesDateDto> {
+    const diariesData = await this.fetchDiariesDateByUser(year, userId);
+    const formattedResult = {};
+
+    await diariesData.forEach((diary) => {
+      const { date, sentiment } = diary;
+      const formattedDate = this.getFormattedDate(date);
+      if (!formattedResult[formattedDate]) {
+        formattedResult[formattedDate] = {
+          sentiment: sentiment,
+          count: 1,
+        };
+      } else {
+        formattedResult[formattedDate].count += 1;
+      }
+    });
+
+    return formattedResult;
   }
 
   private async fetchTopThreeTagsByUser(
@@ -33,6 +61,21 @@ export class StatService {
       .getRawMany();
   }
 
+  private async fetchDiariesDateByUser(
+    year: number,
+    userId: number,
+  ): Promise<DiariesInfoDto[]> {
+    return await Diary.createQueryBuilder("diary")
+      .select(["diary.date", "diary.updatedDate", "diary.sentiment"])
+      .where("diary.user = :userId", { userId })
+      .andWhere("YEAR(diary.date) = :year", { year })
+      .orderBy({
+        "diary.date": "ASC",
+        "diary.updatedDate": "DESC",
+      })
+      .getMany();
+  }
+
   private getFormatResult(result: TagInfoDto[]): StatTagDto {
     const keys = ["first", "second", "third"];
     const formattedResult = {};
@@ -42,5 +85,22 @@ export class StatService {
     });
 
     return formattedResult;
+  }
+
+  private getFormattedDate(date: Date): string {
+    date.setHours(date.getHours() + 9);
+    let formattedDate;
+    formattedDate = date.getFullYear().toString() + "-";
+    formattedDate +=
+      date.getMonth() + 1 < 10
+        ? "0" + (date.getMonth() + 1).toString()
+        : (date.getMonth() + 1).toString();
+    formattedDate += "-";
+    formattedDate +=
+      date.getDate() < 10
+        ? "0" + date.getDate().toString()
+        : date.getDate().toString();
+
+    return formattedDate;
   }
 }
