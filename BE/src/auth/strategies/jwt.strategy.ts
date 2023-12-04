fae93abaa1df94cab1a8035ebc93d62260e28093
JoonSoo-Kim <kjsoo1901@gmail.com>
2023-12-04 21:02:30 +0900

    style: strategies 디렉토리 생성
    - strategies 디렉토리 생성 후 strategy 파일을 배치

--- BE/src/auth/strategies/jwt.strategy.ts
+++ BE/src/auth/strategies/jwt.strategy.ts
@@ -24,6 +24,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any): Promise<User> {
+    console.log("jwtStrategy");
     if (!payload) {
       throw new UnauthorizedException();
     }
