838077d039c267291071c18adf9f7a274c7988a7
JoonSoo-Kim <kjsoo1901@gmail.com>
2023-12-05 21:07:01 +0900

    style: 단위 테스트 디렉토리 생성
    - 단위 테스트 디렉토리와 단위 테스트 실행 파일인 jest-unit.json 생성
    - npm run test:unit으로 단위 테스트를 수행하도록 수정

--- BE/src/purchase/dto/purchase.design.dto.ts
+++ BE/src/purchase/dto/purchase.design.dto.ts
@@ -1,10 +1,17 @@
-import { IsNotEmpty } from "class-validator";
+import { IsEnum, IsIn, IsNotEmpty } from "class-validator";
+import { designEnum, domainEnum } from "src/utils/enum";
 
 export class PurchaseDesignDto {
   @IsNotEmpty({ message: "구매 항목 종류는 비어있지 않아야 합니다." })
+  @IsIn(Object.keys(domainEnum), {
+    message: "적절하지 않은 구매 항목 종류 양식입니다.",
+  })
   domain: string;
 
   @IsNotEmpty({ message: "디자인은 비어있지 않아야 합니다." })
+  @IsIn(Object.keys(designEnum), {
+    message: "적절하지 않은 디자인 양식입니다.",
+  })
   design: string;
 }
 
