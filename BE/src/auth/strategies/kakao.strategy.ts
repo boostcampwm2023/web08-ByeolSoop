7c18641bd7e94642ddc42cca07ef97f62c26b340
jeongmin <yesuel1111@naver.com>
2023-12-05 11:56:36 +0900

    feat: kakaoStrategy DB 저장 및 비밀번호 암호화

--- BE/src/auth/strategies/kakao.strategy.ts
+++ BE/src/auth/strategies/kakao.strategy.ts
@@ -1,14 +1,17 @@
 import "dotenv/config";
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Profile, Strategy } from "passport-kakao";
+import { providerEnum } from "src/utils/enum";
+import { User } from "../users.entity";
+import * as bcrypt from "bcryptjs";
 
 @Injectable()
 export class KakaoStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       clientID: process.env.KAKAO_CLIENT_ID,
-      clientSecret: "",
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
       callbackURL: `${process.env.BACKEND_URL}/auth/kakao/callback`,
     });
   }
@@ -21,15 +24,27 @@ export class KakaoStrategy extends PassportStrategy(Strategy) {
   ) {
     try {
       const { _json } = profile;
-      console.log(_json);
-      const user = {
-        kakaoId: _json.id,
-        nickname: _json.properties.nickname,
-        email: _json.kakao_account.email,
-      };
+      const { id, kakao_account, properties } = _json;
+
+      const user = new User();
+
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(id.toString(), salt);
+
+      user.email = kakao_account.email;
+      user.userId = id.toString() + "*kakao";
+      user.nickname = properties.nickname;
+      user.password = hashedPassword;
+      user.provider = providerEnum.KAKAO;
+
       done(null, user);
     } catch (error) {
-      done(error);
+      console.log(error);
+      done(
+        new BadRequestException(
+          `카카오 로그인 중 오류가 발생했습니다 : ${error.message}`,
+        ),
+      );
     }
   }
 }
