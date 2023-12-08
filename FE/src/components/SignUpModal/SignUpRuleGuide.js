3713fe115d80e7c48e8142107ce4ae8229e7f133
dmson1218 <dmson1218@naver.com>
2023-12-09 01:09:56 +0900

    feat: 회원 가입 규칙 안내문 추가

---
+++ FE/src/components/SignUpModal/SignUpRuleGuide.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import rule from "../../assets/rule.png";
+
+function SignUpRuleGuide() {
+  const [isRuleGuideOpen, setIsRuleGuideOpen] = useState(false);
+  return (
+    <SignUpRuleGuideWrapper>
+      <SignUpRuleGuideImg
+        src={rule}
+        alt='rule'
+        onMouseEnter={() => setIsRuleGuideOpen(true)}
+        onMouseLeave={() => setIsRuleGuideOpen(false)}
+      />
+      {isRuleGuideOpen && (
+        <SignUpRuleGuideTextWrapper>
+          <SignUpRuleGuideText>
+            아이디: 5~20자의 영문 대소문자, 숫자와 특수기호(_),(-)
+            <br />
+            비밀번호: 5~20자의 영문 대소문자, 숫자, 특수문자
+          </SignUpRuleGuideText>
+        </SignUpRuleGuideTextWrapper>
+      )}
+    </SignUpRuleGuideWrapper>
+  );
+}
+
+const SignUpRuleGuideTextWrapper = styled.div`
+  width: 75%;
+  height: 15%;
+  border-radius: 0.5rem;
+  background-color: #bbc2d4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 14%;
+  font-size: 1rem;
+  text-align: center;
+  line-height: 1.8rem;
+
+  animation: fadeIn 0.3s forwards;
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+const SignUpRuleGuideWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SignUpRuleGuideImg = styled.img`
+  width: 1.8rem;
+  filter: invert(100%);
+  cursor: pointer;
+`;
+
+const SignUpRuleGuideText = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: black;
+`;
+
+export default SignUpRuleGuide;
--- FE/src/styles/Modal/ModalTitle.js
+++ FE/src/styles/Modal/ModalTitle.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const ModalTitle = styled.div`
   font-size: 2.25rem;
   font-weight: bold;
-  margin-bottom: 1rem;
 `;
 
 export default ModalTitle;
