1d4fb98abc1ad202d0e39b39c0c73d70220b5dec
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2023-12-11 03:31:33 +0900

    fix: 주석 제거

--- FE/src/pages/HomePage.js
+++ FE/src/pages/HomePage.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { useRecoilValue } from "recoil";
 import headerAtom from "../atoms/headerAtom";
-// import homeBackground from "../assets/homeBackground.png";
 import LoginModal from "../components/LoginModal/LoginModal";
 import SignUpModal from "../components/SignUpModal/SignUpModal";
 
@@ -20,19 +19,6 @@ function HomePage() {
     </>
   );
 }
-
-// TODO: 배경 이미지 제거하고 영상으로 대체할 것
-// const HomePageWrapper = styled.div`
-//   height: 100vh;
-//   background-image: url(${homeBackground});
-//   background-repeat: no-repeat;
-//   background-position: center;
-//   background-size: cover;
-
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
 
 const HomepageBackground = styled.video`
   position: absolute;
