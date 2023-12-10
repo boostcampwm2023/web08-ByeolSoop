c3f7e1a2660f15ef29d02b378d5ece3c70ae1342
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2023-12-11 03:25:25 +0900

    chore: 홈페이지 배경 화면 영상으로 대체

--- FE/src/pages/HomePage.js
+++ FE/src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { useRecoilValue } from "recoil";
 import headerAtom from "../atoms/headerAtom";
-import homeBackground from "../assets/homeBackground.png";
+// import homeBackground from "../assets/homeBackground.png";
 import LoginModal from "../components/LoginModal/LoginModal";
 import SignUpModal from "../components/SignUpModal/SignUpModal";
 
@@ -11,7 +11,9 @@ function HomePage() {
 
   return (
     <>
-      <HomePageWrapper />
+      <HomepageBackground autoPlay loop muted>
+        <source src='/homepage.mp4' type='video/mp4' />
+      </HomepageBackground>
 
       {headerState.isLogin ? <LoginModal /> : null}
       {headerState.isSignUp ? <SignUpModal /> : null}
@@ -20,16 +22,24 @@ function HomePage() {
 }
 
 // TODO: 배경 이미지 제거하고 영상으로 대체할 것
-const HomePageWrapper = styled.div`
-  height: 100vh;
-  background-image: url(${homeBackground});
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
+// const HomePageWrapper = styled.div`
+//   height: 100vh;
+//   background-image: url(${homeBackground});
+//   background-repeat: no-repeat;
+//   background-position: center;
+//   background-size: cover;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+const HomepageBackground = styled.video`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
 `;
 
 export default HomePage;
