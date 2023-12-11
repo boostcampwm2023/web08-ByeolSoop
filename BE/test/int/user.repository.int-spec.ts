883d92f4c6c27f189d39cadcda132fdf08654761
jeongmin <yesuel1111@naver.com>
2023-12-11 23:11:18 +0900

    test: user repository 코드 수정
    - 중복된 이메일 테스트 코드 제거
    - 존재하지 않는 유저 getUserByUserId로 조회
    - commonUser, 네이버 유저, 카카오 유저에 대한 getUserByUserId 검사

--- BE/test/int/user.repository.int-spec.ts
+++ BE/test/int/user.repository.int-spec.ts
@@ -9,6 +9,7 @@ import { ConflictException } from "@nestjs/common";
 import { User } from "src/auth/users.entity";
 import { DataSource } from "typeorm";
 import { TransactionalTestContext } from "typeorm-transactional-tests";
+import { premiumStatus, providerEnum } from "src/utils/enum";
 
 describe("UsersRepository 통합 테스트", () => {
   let usersRepository: UsersRepository;
@@ -53,9 +54,14 @@ describe("UsersRepository 통합 테스트", () => {
       createUserDto.nickname = "ValidNickname";
 
       const result = await usersRepository.createUser(createUserDto);
-
       expect(result).toBeInstanceOf(User);
-      expect(result.userId).toBe("ValidUser123");
+      expect(result).toMatchObject({
+        userId: "ValidUser123",
+        email: "valid.email@test.com",
+        provider: providerEnum.BYEOLSOOP,
+        credit: 0,
+        premium: premiumStatus.FALSE,
+      });
     });
 
     it("중복된 아이디로 요청 시 실패", async () => {
@@ -69,32 +75,62 @@ describe("UsersRepository 통합 테스트", () => {
         await usersRepository.createUser(createUserDto);
       } catch (error) {
         expect(error).toBeInstanceOf(ConflictException);
-      }
-    });
-
-    it("중복된 이메일로 요청 시 실패", async () => {
-      const createUserDto = new CreateUserDto();
-      createUserDto.userId = "ValidUser1234";
-      createUserDto.email = "valid.email@test.com";
-      createUserDto.password = "ValidPass123!";
-      createUserDto.nickname = "ValidNickname";
-
-      try {
-        await usersRepository.createUser(createUserDto);
-      } catch (error) {
-        expect(error).toBeInstanceOf(ConflictException);
+        expect(error.message).toBe("중복된 아이디입니다.");
       }
     });
   });
 
   describe("getUserByUserId 메서드", () => {
-    it("메서드 정상 요청", async () => {
+    it("commonUser 메서드 정상 요청", async () => {
       const userId = "commonUser";
 
       const result = await usersRepository.getUserByUserId(userId);
 
       expect(result).toBeInstanceOf(User);
-      expect(result.email).toBe("byeolsoop08@naver.com");
+      expect(result).toMatchObject({
+        userId: "commonUser",
+        email: "byeolsoop08@naver.com",
+        provider: providerEnum.BYEOLSOOP,
+        credit: 0,
+        premium: premiumStatus.FALSE,
+      });
+    });
+
+    it("네이버유저 메서드 정상 요청", async () => {
+      const userId = "naverUser";
+
+      const result = await usersRepository.getUserByUserId(userId);
+
+      expect(result).toBeInstanceOf(User);
+      expect(result).toMatchObject({
+        userId: "naverUser",
+        email: "naverUser@naver.com",
+        provider: providerEnum.NAVER,
+        credit: 500,
+        premium: premiumStatus.FALSE,
+      });
+    });
+
+    it("카카오유저 메서드 정상 요청", async () => {
+      const userId = "kakaoUser";
+
+      const result = await usersRepository.getUserByUserId(userId);
+
+      expect(result).toBeInstanceOf(User);
+      expect(result).toMatchObject({
+        userId: "kakaoUser",
+        email: "kakaoUser@kakao.com",
+        provider: providerEnum.KAKAO,
+        credit: 1000,
+        premium: premiumStatus.TRUE,
+      });
+    });
+
+    it("존재하지 않는 사용자 id 조회", async () => {
+      const userId = "notFoundUser";
+
+      const result = await usersRepository.getUserByUserId(userId);
+      expect(result).toBeNull();
     });
   });
 });
