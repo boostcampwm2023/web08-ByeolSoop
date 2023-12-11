971df0af6323cba11388c7d40d6f4233c3522547
jeongmin <yesuel1111@naver.com>
2023-12-12 01:56:48 +0900

    test: ShapesRepository 통합 테스트 수정
    - DB에 저장된 정보 사용
    - 제대로 돌아가지 않던 에러 확인 테스트 삭제

--- BE/test/int/shapes.repository.int-spec.ts
+++ BE/test/int/shapes.repository.int-spec.ts
@@ -1,10 +1,9 @@
-import { RedisModule } from "@liaoliaots/nestjs-redis";
+import { NotFoundException } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "src/auth/users.entity";
 import { typeORMTestConfig } from "src/configs/typeorm.test.config";
 import { Shape } from "src/shapes/shapes.entity";
-import { ShapesModule } from "src/shapes/shapes.module";
 import { ShapesRepository } from "src/shapes/shapes.repository";
 import { DataSource } from "typeorm";
 import { TransactionalTestContext } from "typeorm-transactional-tests";
@@ -13,25 +12,19 @@ describe("ShapesRepository 통합 테스트", () => {
   let shapesRepository: ShapesRepository;
   let dataSource: DataSource;
   let transactionalContext: TransactionalTestContext;
+  let commonUser: User;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRoot(typeORMTestConfig),
-        ShapesModule,
-        RedisModule.forRoot({
-          readyLog: true,
-          config: {
-            host: "223.130.129.145",
-            port: 6379,
-          },
-        }),
-      ],
+      imports: [TypeOrmModule.forRoot(typeORMTestConfig)],
       providers: [ShapesRepository],
     }).compile();
 
     shapesRepository = moduleFixture.get<ShapesRepository>(ShapesRepository);
     dataSource = moduleFixture.get<DataSource>(DataSource);
+    commonUser = await User.findOne({
+      where: { userId: "commonUser" },
+    });
   });
 
   beforeEach(async () => {
@@ -47,60 +40,44 @@ describe("ShapesRepository 통합 테스트", () => {
 
   describe("createDefaultShapes 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
-      const commonUser = await User.findOne({
-        where: { userId: "commonUser" },
-      });
+      await dataSource.query("SET FOREIGN_KEY_CHECKS = 0");
+      await Shape.delete({});
+      await dataSource.query("SET FOREIGN_KEY_CHECKS = 1");
+      expect(await Shape.find()).toHaveLength(0);
 
       await shapesRepository.createDefaultShapes(commonUser);
+      expect(await Shape.find()).toHaveLength(15);
     });
   });
 
   describe("getShapesByUser 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
-      const commonUser = await User.findOne({
-        where: { userId: "commonUser" },
-      });
-
       const result: Shape[] =
-        await shapesRepository.getShapesByUser(commonUser);
+        await shapesRepository.getShapesByUser("commonUser");
 
-      // 현재는 기본 모양 15개와 기존에 임시로 사용하던 기본 모양 3개가 모두 있음
-      // 추후 임시 모양 삭제 시 15로 수정할 것
-      expect(result.length).toBe(15);
+      expect(result).toHaveLength(15);
     });
   });
 
   describe("getShapeByShapePath 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
-      const shape = await Shape.findOne({
-        where: {
-          user: {
-            userId: "commonUser",
-          },
-        },
-      });
+      const shapePath = "BasicShape10.svg";
+      const result = await shapesRepository.getShapeByShapePath(shapePath);
 
-      const result = await shapesRepository.getShapeByShapePath(
-        shape.shapePath,
-      );
-
-      expect(result).toStrictEqual(shape);
+      expect(result.shapePath).toEqual(shapePath);
+      expect(result.uuid).toBe("c048ba4f-4ea9-4605-bcc9-e45103d68a4f");
+      expect(result.user.userId).toBe("commonUser");
     });
   });
 
   describe("getShapeByUuid 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
-      const shape = await Shape.findOne({
-        where: {
-          user: {
-            userId: "commonUser",
-          },
-        },
-      });
+      const uuid = "c048ba4f-4ea9-4605-bcc9-e45103d68a4f";
+      const result = await shapesRepository.getShapeByUuid(uuid);
 
-      const result = await shapesRepository.getShapeByUuid(shape.uuid);
-
-      expect(result).toStrictEqual(shape);
+      expect(result.uuid).toEqual(uuid);
+      expect(result.shapePath).toBe("BasicShape10.svg");
+      expect(result.user.userId).toBe("commonUser");
     });
   });
 });
