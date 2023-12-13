cbf0a3284ab0f7a782616eaeb645e2bbefee2259
jeongmin <yesuel1111@naver.com>
2023-12-13 18:42:20 +0900

    test: DiariesService 통합 테스트 수정
    - 사용하지 않는 모듈 제거
    - beforeAll에 중복되는 모킹 추가
    - afterAll에 restoreAllMocks 함수 호출
    - 테스트 DB 더미 데이터 반영

--- BE/test/int/diaries.service.int-spec.ts
+++ BE/test/int/diaries.service.int-spec.ts
@@ -1,10 +1,8 @@
-import { RedisModule } from "@liaoliaots/nestjs-redis";
 import { Test, TestingModule } from "@nestjs/testing";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "src/auth/users.entity";
 import { typeORMTestConfig } from "src/configs/typeorm.test.config";
 import { Diary } from "src/diaries/diaries.entity";
-import { DiariesModule } from "src/diaries/diaries.module";
 import { DiariesRepository } from "src/diaries/diaries.repository";
 import {
   CreateDiaryDto,
@@ -24,121 +22,104 @@ import { createCipheriv, createDecipheriv } from "crypto";
 import { of } from "rxjs";
 import { DataSource } from "typeorm";
 import { TransactionalTestContext } from "typeorm-transactional-tests";
+import { UsersRepository } from "src/auth/users.repository";
+import { NotFoundException } from "@nestjs/common";
 
 describe("DiariesService 통합 테스트", () => {
   let diariesService: DiariesService;
-  let tagsRepository: TagsRepository;
+  let diariesRepository: DiariesRepository;
   let sentimentDto: SentimentDto;
   let httpService: HttpService;
-  let user: User;
-  let shape: Shape;
-  let createResult: Diary;
   let dataSource: DataSource;
   let transactionalContext: TransactionalTestContext;
 
-  jest.mock("rxjs", () => ({
-    lastValueFrom: jest.fn().mockResolvedValue({
-      data: {
-        document: {
-          confidence: {
-            positive: 0.1,
-            neutral: 0.2,
-            negative: 0.7,
-          },
-          sentiment: "negative",
-        },
-      },
-    }),
-  }));
-
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRoot(typeORMTestConfig),
-        HttpModule,
-        DiariesModule,
-        RedisModule.forRoot({
-          readyLog: true,
-          config: {
-            host: "223.130.129.145",
-            port: 6379,
-          },
-        }),
-      ],
+      imports: [TypeOrmModule.forRoot(typeORMTestConfig), HttpModule],
       providers: [
         DiariesService,
         DiariesRepository,
         TagsRepository,
         ShapesRepository,
-        {
-          provide: HttpService,
-          useValue: {
-            post: jest.fn(),
-          },
-        },
+        UsersRepository,
       ],
     }).compile();
 
     diariesService = moduleFixture.get<DiariesService>(DiariesService);
-    tagsRepository = moduleFixture.get<TagsRepository>(TagsRepository);
+    diariesRepository = moduleFixture.get<DiariesRepository>(DiariesRepository);
     httpService = moduleFixture.get<HttpService>(HttpService);
     dataSource = moduleFixture.get<DataSource>(DataSource);
+
+    sentimentDto = new SentimentDto(0, 100, 0, sentimentStatus.NEUTRAL);
+
+    jest
+      .spyOn(diariesService, "getSentiment")
+      .mockResolvedValueOnce(sentimentDto);
+
+    const mockData: any = {
+      status: 200,
+      statusText: "OK",
+      headers: {},
+      config: {},
+      data: {
+        document: {
+          confidence: {
+            positive: 10.0,
+            neutral: 20.0,
+            negative: 70.0,
+          },
+          sentiment: "negative",
+        },
+      },
+    };
+    jest.spyOn(httpService, "post").mockImplementation(() => of(mockData));
   });
 
   beforeEach(async () => {
     transactionalContext = new TransactionalTestContext(dataSource);
     await transactionalContext.start();
-
-    sentimentDto = new SentimentDto();
-    sentimentDto.positiveRatio = 0;
-    sentimentDto.negativeRatio = 0;
-    sentimentDto.neutralRatio = 100;
-    sentimentDto.sentiment = sentimentStatus.NEUTRAL;
-    user = await User.findOne({ where: { userId: "commonUser" } });
-    shape = await Shape.findOne({
-      where: { user: { userId: "commonUser" } },
-    });
-
-    jest
-      .spyOn(diariesService, "getTags")
-      .mockResolvedValueOnce([new Tag(), new Tag()]);
-    jest
-      .spyOn(diariesService, "getSentiment")
-      .mockResolvedValueOnce(sentimentDto);
-
-    const createDiaryDto = new CreateDiaryDto();
-    createDiaryDto.title = "Test Title";
-    createDiaryDto.content = "Test Content";
-    createDiaryDto.point = "1,1,1";
-    (createDiaryDto.date as any) = "2023-11-29";
-    createDiaryDto.tags = ["tag1", "tag2"];
-    createDiaryDto.shapeUuid = shape.uuid;
-
-    createResult = await diariesService.writeDiary(createDiaryDto, user);
   });
 
   afterEach(async () => {
     await transactionalContext.finish();
+  });
+
+  afterAll(async () => {
     await jest.restoreAllMocks();
   });
 
   describe("writeDiary 통합 테스트", () => {
-    it("메서드 정상 요청", async () => {
-      expect(createResult).toBeInstanceOf(Diary);
-      expect(createResult.title).toBe("Test Title");
-      expect(createResult).toMatchObject({
+    it("신규 유저 메서드 정상 요청", async () => {
+      const newUser = await User.findOne({ where: { userId: "newUser" } });
+      const shape = await Shape.findOne({ where: { id: 2 } });
+
+      const createDiaryDto = new CreateDiaryDto();
+      createDiaryDto.title = "Test Title";
+      createDiaryDto.content = "Test Content";
+      createDiaryDto.point = "1,1,1";
+      createDiaryDto.date = new Date("2023-11-29");
+      createDiaryDto.tags = ["Naver"];
+      createDiaryDto.shapeUuid = shape.uuid;
+
+      const result = await diariesService.writeDiary(createDiaryDto, newUser);
+      expect(result).toBeInstanceOf(Diary);
+      expect(result).toMatchObject({
         title: "Test Title",
-        content: "f2302664806f9f519404b1d583902d36",
         positiveRatio: 0,
         negativeRatio: 0,
         neutralRatio: 100,
         sentiment: "neutral",
-        date: "2023-11-29",
+        date: new Date("2023-11-29"),
         point: "1,1,1",
-        user: { id: 1 },
-        shape: { id: 1 },
+        user: { id: 3 },
+        shape: { id: 2 },
         deletedDate: null,
       });
+
+      const decryptedContent = await diariesService.getDecryptedContent(
+        result.content,
+      );
+      expect(decryptedContent).toBe("Test Content");
     });
   });
 
@@ -179,101 +160,122 @@ describe("DiariesService 통합 테스트", () => {
   describe("readDiary 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
       const readDiaryDto = new ReadDiaryDto();
-      readDiaryDto.uuid = createResult.uuid;
-
-      jest
-        .spyOn(diariesService, "getDecryptedContent")
-        .mockResolvedValue("decrypted");
+      readDiaryDto.uuid = "0c388311-61c4-41ad-8d4d-b7b1ce075ef2";
 
       const result = await diariesService.readDiary(readDiaryDto);
 
       expect(result).toBeInstanceOf(Diary);
-      expect(result.title).toBe("Test Title");
+      expect(result).toMatchObject({
+        title: "부정 1",
+        content: "너무 싫어",
+        uuid: "0c388311-61c4-41ad-8d4d-b7b1ce075ef2",
+        positiveRatio: 0.00949954,
+        negativeRatio: 99.978,
+        neutralRatio: 0.0124967,
+        sentiment: "negative",
+        date: new Date("2023-03-03"),
+        tags: [
+          { id: 1, name: "Old" },
+          { id: 5, name: "9월" },
+          { id: 8, name: "중립" },
+        ],
+      });
     });
   });
 
   describe("readDiariesByUser 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
-      jest
-        .spyOn(diariesService, "getDecryptedContent")
-        .mockResolvedValue("decrypted2323");
+      const naverUser = await User.findOne({ where: { userId: "naverUser" } });
+      const diaries = await diariesService.readDiariesByUser(naverUser);
 
-      const result = await diariesService.readDiariesByUser(user);
+      const expectedValues = [
+        { title: "긍정 네이버 1", content: "네이버는 너무 좋아" },
+        { title: "긍정 네이버 2", content: "네이버는 너무 행복해" },
+        { title: "긍정 네이버 3", content: "행복해" },
+      ];
 
-      expect(result[0]).toBeInstanceOf(Diary);
+      expect(
+        diaries.map((diary) => ({
+          title: diary.title,
+          content: diary.content,
+        })),
+      ).toEqual(expectedValues);
     });
   });
 
   describe("modifyDiary 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
+      const oldUser = await User.findOne({ where: { userId: "oldUser" } });
+      const shape = await Shape.findOne({ where: { id: 3 } });
+
       const updateDiaryDto = new UpdateDiaryDto();
       updateDiaryDto.title = "Test Title";
-      updateDiaryDto.content = "Test Content";
+      updateDiaryDto.content = "Updated Content";
       updateDiaryDto.point = "3,3,3";
-      (updateDiaryDto.date as any) = "2023-11-29";
-      updateDiaryDto.tags = ["tag1", "tag2"];
+      updateDiaryDto.date = new Date("2023-11-29");
+      updateDiaryDto.tags = ["Naver"];
       updateDiaryDto.shapeUuid = shape.uuid;
-      updateDiaryDto.uuid = createResult.uuid;
+      updateDiaryDto.uuid = "0c388311-61c4-41ad-8d4d-b7b1ce075ef2";
 
-      const result = await diariesService.modifyDiary(updateDiaryDto, user);
+      const result = await diariesService.modifyDiary(updateDiaryDto, oldUser);
 
       expect(result).toBeInstanceOf(Diary);
-      expect(result.point).toBe("3,3,3");
+      expect(result).toMatchObject({
+        title: "Test Title",
+        uuid: "0c388311-61c4-41ad-8d4d-b7b1ce075ef2",
+        positiveRatio: 10,
+        neutralRatio: 20,
+        negativeRatio: 70,
+        sentiment: "negative",
+        date: new Date("2023-11-29"),
+        tags: [{ id: 13, name: "Naver" }],
+        point: "3,3,3",
+      });
+
+      const decryptedContent = await diariesService.getDecryptedContent(
+        result.content,
+      );
+      expect(decryptedContent).toBe("Updated Content");
     });
   });
 
   describe("deleteDiary 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
       const deleteDiaryDto = new DeleteDiaryDto();
-      deleteDiaryDto.uuid = createResult.uuid;
+      deleteDiaryDto.uuid = "0c388311-61c4-41ad-8d4d-b7b1ce075ef2";
 
       await diariesService.deleteDiary(deleteDiaryDto);
+
+      try {
+        await diariesRepository.getDiaryByUuid(deleteDiaryDto.uuid);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toBe("존재하지 않는 일기입니다.");
+      }
     });
   });
 
   describe("getTags 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
-      const tagNames = ["tag", "tag"];
-      const tag = new Tag();
-      tag.name = "tag";
-
-      jest.spyOn(tagsRepository, "getTagByName").mockResolvedValue(tag);
-      jest.spyOn(tagsRepository, "createTag").mockResolvedValue(tag);
+      const tagNames = ["3월", "5월", "7월", "13월"];
 
       const result = await diariesService.getTags(tagNames);
 
       expect(result[0]).toBeInstanceOf(Tag);
-      expect(result[0].name).toBe("tag");
+      expect(result.map((tag) => tag.name)).toEqual(
+        expect.arrayContaining(tagNames),
+      );
     });
   });
 
   describe("getSentimentByContent 통합 테스트", () => {
     it("메서드 정상 요청", async () => {
-      const result: any = {
-        status: 200,
-        statusText: "OK",
-        headers: {},
-        config: {},
-        data: {
-          document: {
-            confidence: {
-              positive: 0.1,
-              neutral: 0.2,
-              negative: 0.7,
-            },
-            sentiment: "negative",
-          },
-        },
-      };
-
-      jest.spyOn(httpService, "post").mockImplementation(() => of(result));
-
       const sentimentDto = await diariesService.getSentimentByContent("test");
 
       expect(sentimentDto).toEqual({
-        positiveRatio: 0.1,
-        neutralRatio: 0.2,
-        negativeRatio: 0.7,
+        positiveRatio: 10,
+        neutralRatio: 20,
+        negativeRatio: 70,
         sentiment: "negative",
       });
     });
@@ -281,59 +283,21 @@ describe("DiariesService 통합 테스트", () => {
 
   describe("getSentiment 통합 테스트", () => {
     it("1000자 이하 메서드 정상 요청", async () => {
-      const result: any = {
-        status: 200,
-        statusText: "OK",
-        headers: {},
-        config: {},
-        data: {
-          document: {
-            confidence: {
-              positive: 0.1,
-              neutral: 0.2,
-              negative: 0.7,
-            },
-            sentiment: "negative",
-          },
-        },
-      };
-
-      jest.spyOn(httpService, "post").mockImplementation(() => of(result));
-
       const sentimentDto = await diariesService.getSentiment("test");
 
       expect(sentimentDto).toEqual({
-        positiveRatio: 0.1,
-        neutralRatio: 0.2,
-        negativeRatio: 0.7,
+        positiveRatio: 10,
+        neutralRatio: 20,
+        negativeRatio: 70,
         sentiment: "negative",
       });
     });
 
     it("1000자 초과 메서드 정상 요청", async () => {
-      const result: any = {
-        status: 200,
-        statusText: "OK",
-        headers: {},
-        config: {},
-        data: {
-          document: {
-            confidence: {
-              positive: 0.1,
-              neutral: 0.2,
-              negative: 0.7,
-            },
-            sentiment: "negative",
-          },
-        },
-      };
-
-      jest.spyOn(httpService, "post").mockImplementation(() => of(result));
-
       const sentimentDto = await diariesService.getSentiment("a".repeat(1500));
 
-      expect(sentimentDto.negativeRatio).toBeGreaterThan(0.69);
-      expect(sentimentDto.negativeRatio).toBeLessThan(0.71);
+      expect(sentimentDto.negativeRatio).toBeGreaterThan(69);
+      expect(sentimentDto.negativeRatio).toBeLessThan(71);
       expect(sentimentDto.sentiment).toBe("negative");
     });
   });
